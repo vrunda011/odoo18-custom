98a3ec81c2fca45ebf87c42977b8214938afe06c
Vrunda Patel <vrundapatel@Vrundas-MacBook-Air.local>
2025-04-15 12:09:46 +0530

    [IMP] updated sale approve module.

--- training/sale_approval/models/res_config_settings.py
+++ training/sale_approval/models/res_config_settings.py
@@ -7,7 +7,6 @@ class ResConfigSettings(models.TransientModel):
     so_double_validation = fields.Selection(related='company_id.so_double_validation', string="Levels of Approvals *", readonly=False)
     so_double_validation_amount = fields.Monetary(related='company_id.so_double_validation_amount', string="Minimum Amount", currency_field='company_currency_id', readonly=False)
 
-
     def set_values(self):
         super().set_values()
         so_double_validation = 'two_step' if self.so_order_approval else 'one_step'
--- training/sale_approval/models/sale_order.py
+++ training/sale_approval/models/sale_order.py
@@ -16,6 +16,21 @@ class SaleOrder(models.Model):
             else:
                 order.write({'state': 'to approve'})
 
+    def _confirmation_error_message(self):
+        """ Return whether order can be confirmed or not if not then returm error message. """
+        self.ensure_one()
+        if self.state not in {'draft', 'sent', 'to approve'}:
+            return "Some orders are not in a state requiring confirmation."
+        if any(
+            not line.display_type
+            and not line.is_downpayment
+            and not line.product_id
+            for line in self.order_line
+        ):
+            return "A line on these orders missing a product, you cannot confirm it."
+
+        return False
+
     def button_approve(self, force=False):
         self = self.filtered(lambda order: order._approval_allowed())
         self.write({'state': 'sale', 'date_order': fields.Datetime.now()})
@@ -25,9 +40,9 @@ class SaleOrder(models.Model):
         """Returns whether the order qualifies to be approved by the current user"""
         self.ensure_one()
         return (
-            self.company_id.po_double_validation == 'one_step'
-            or (self.company_id.po_double_validation == 'two_step'
+            self.company_id.so_double_validation == 'one_step'
+            or (self.company_id.so_double_validation == 'two_step'
                 and self.amount_total < self.env.company.currency_id._convert(
-                    self.company_id.po_double_validation_amount, self.currency_id, self.company_id,
+                    self.company_id.so_double_validation_amount, self.currency_id, self.company_id,
                     self.date_order or fields.Date.today()))
             or self.env.user.has_group('sales_team.group_sale_manager'))
