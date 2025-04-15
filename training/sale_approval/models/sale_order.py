4d8c8b23d2f8d3d7fd5f10cd558ab8fcc1727cd8
Vrunda Patel <vrundapatel@Vrundas-MacBook-Air.local>
2025-04-15 12:28:20 +0530

    [IMP] update button_approve method.

--- training/sale_approval/models/sale_order.py
+++ training/sale_approval/models/sale_order.py
@@ -32,10 +32,11 @@ class SaleOrder(models.Model):
         return False
 
     def button_approve(self, force=False):
-        res = super(SaleOrder, self).action_confirm()
-        self = self.filtered(lambda order: order._approval_allowed())
-        self.write({'state': 'sale', 'date_order': fields.Datetime.now()})
-        return res
+        # res = super(SaleOrder, self).action_confirm()
+        to_approve_orders = self.filtered(lambda order: order._approval_allowed())
+        # self.write({'state': 'sale', 'date_order': fields.Datetime.now()})
+        if to_approve_orders:
+            super(SaleOrder, self).action_confirm()
 
     def _approval_allowed(self):
         """Returns whether the order qualifies to be approved by the current user"""
